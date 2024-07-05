70958fb3b27bbe0194f006c16fb5b0d601bf6ecd
Braian Sins <bsins198@gmail.com>
2024-07-05 17:32:36 -0300

    adicionando views e rotas

--- routes/web.php
+++ routes/web.php
@@ -2,29 +2,35 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
-//antes era pra ser apenas Route::get('/', 'PrincipalController@principal');
-//porém a ação se fez necessário devivo a mudança de versão do laravel 8
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
 
-
-//C:\Users\braian.sins\app_super_gestao\app\Http\Controllers\PrincipalController.php
-//Route::get('/', function () {
-//    return 'Olá, seja bem vindo ao curso!';
-//});
-
-Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
 //mesma coisa do de cima, porém com a rota de sobre-nos
 
 
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
 
-Route::get('/contato/{nome?}/{categoria_id?}/{assunto?}/{mensagem?}',
-function (
-string $nome = 'Nome não informado',
-int $categoria_id = 1,
-string $assunto = 'Assunto não informado',
-string $mensagem = 'mensagem não informada') {
-    return "Estamos aqui: $nome - $categoria_id - $assunto - $mensagem";
-}
 
-)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
+Route::get('/login', function () { return 'Login'; })->name('site.login');
+
+
+//relação de dependência entre rotas. agrupamento de rotas pelo prefixo /app
+Route::prefix('/app')->group(function () {
+
+        Route::get('/clientes', function () { return 'Clientes';})->name('app.clientes');
+
+        Route::get('/fornecedores', function () { return 'Fornecedores';})->name('app.fornecedores');
+
+        Route::get('/produtos', function () { return 'Produtos';})->name('app.produtos');
+
+    });
+
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
+
+
+Route::fallback(function () {
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para ir para a página inicial';
+});
+
+
+
